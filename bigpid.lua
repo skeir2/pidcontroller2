eaf34b3aac104b81d383d8751e2da5c42a239804
skeir2 <phong2k06@gmail.com>
2026-04-22 15:34:31 -0500

    pid factory

--- bigpid.lua
+++ bigpid.lua
@@ -1,4 +1,17 @@
-function onTick(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
+local DELTA_TIME = 0.1
+local pidfactory = require("pidfactory")
+
+-- init
+local gimbal = "left"
+local FR_SpeedController = "Create_RotationSpeedController_2"
+local FL_SpeedController = "Create_RotationSpeedController_0"
+local BR_SpeedController = "Create_RotationSpeedController_1"
+local BL_SpeedController = "Create_RotationSpeedController_3"
+
+local roll_pid = pidfactory.create_pid(0.5, 0, 0, 0)
+local pitch_pid = pidfactory.create_pid(0.5, 0, 0, 0)
+
+function onTick()
     local controllers = {
         FR = FR_SpeedController, 
         FL = FL_SpeedController, 
@@ -8,21 +21,17 @@ function onTick(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedControll
 
     for positionName, controllerName in pairs(controllers) do
         local controller = peripheral.wrap(controllerName)
-        print(string.format("%s, %s: %d", positionName, controllerName, controller.getTargetSpeed()))
+        print(string.format("%s: %d", positionName, controller.getTargetSpeed()))
+    end
+
+    print(roll_pid:tick(1, 0, DELTA_TIME))
+end
+
+function init()
+    while true do
+        onTick()
+        sleep(DELTA_TIME)
     end
 end
 
-function init(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
-    while true do
-        onTick(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
-        sleep(0.1)
-    end
-end
- 
--- initialize here
-local gimbal = "left"
-local FR_SpeedController = "Create_RotationSpeedController_2"
-local FL_SpeedController = "Create_RotationSpeedController_0"
-local BR_SpeedController = "Create_RotationSpeedController_1"
-local BL_SpeedController = "Create_RotationSpeedController_3"
-init(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
+init()
---
+++ pidfactory.lua
@@ -0,0 +1,35 @@
+local function clamp(num,low,high)
+    num = math.min(num,high)
+    num = math.max(num,low)
+    return num
+end
+
+local function create_pid(kp, ki, kd, bias)
+    local pid = {
+        kp = kp,
+        ki = ki,
+        kd = kd,
+        bias = bias,
+        error_prior = 0,
+        integral_prior = 0,
+        tick = function(self, setpoint, getpoint, delta_time)
+            local setRps = setpoint
+            local getRps = getpoint
+        
+            local error = setRps - getRps
+            local integral = clamp(self.integral_prior+error*0.1,-100/self.ki,100/self.ki)
+            local derivative = (error-self.error_prior)/0.1
+        
+            local value_out = self.kp*error+self.ki*integral+self.kd*derivative+bias
+        
+            self.error_prior = error
+            self.integral_prior = integral
+        
+            return value_out
+        end
+    }
+
+    return pid
+end
+
+return { create_pid = create_pid }
