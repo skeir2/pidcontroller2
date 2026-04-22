523b012c71e8ec5a9baa6f6f84c1959edeb7ff2d
skeir2 <phong2k06@gmail.com>
2026-04-22 15:14:42 -0500

    1111

--- bigpid.lua
+++ bigpid.lua
@@ -1,1 +1,28 @@
-print("hi")
+function onTick(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
+    local controllers = {
+        FR_SpeedController, 
+        FL_SpeedController, 
+        BR_SpeedController, 
+        BL_SpeedController
+    }
+
+    for i, controllerName in pairs(controllers) do
+        local controller = peripheral.wrap(controllerName)
+        print(string.format("%s: %d", controllerName, controller.getTargetSpeed()))
+    end
+end
+
+function init(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
+    while true do
+        onTick(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
+        sleep(0.1)
+    end
+end
+ 
+-- initialize here
+local gimbal = "left"
+local FR_SpeedController = "Create_RotationSpeedController_2"
+local FL_SpeedController = "Create_RotationSpeedController_0"
+local BR_SpeedController = "Create_RotationSpeedController_1"
+local BL_SpeedController = "Create_RotationSpeedController_3"
+init(gimbal, FR_SpeedController, FL_SpeedController, BR_SpeedController, BL_SpeedController)
